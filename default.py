661f2a6623e27b9262f3ca5fe79748c0f2942d00
Lavender Starlight <56975081+faithvoid@users.noreply.github.com>
2024-04-24 04:18:02 -0400

    Update default.py

--- default.py
+++ default.py
@@ -1,2 +1,2 @@
-xbmc.executebuiltin('XBMC.RunScript(Q:\\scripts\\CortanaChat\\notifycc.py)')
+xbmc.executebuiltin('XBMC.RunScript(Q:\\scripts\\CortanaChat\\notify.py)')
 xbmc.executebuiltin('XBMC.RunScript(Q:\\scripts\\CortanaChat\\menu.py)')
